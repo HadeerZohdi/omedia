04631b809200927580120c6039cd2f4b3a8183c9
HadeerZohdi <hadeer.zohdi@gmail.com>
2024-12-02 20:11:17 +0300

    fix-responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,16 @@ import logo from "../assets/Full Logo-02.png"
 function Home(){
 
     return  (
-        <div className="home-box">
-            <img src={logo} alt="OMedia" />
-            <p className="header-text">WOW is Comming Soon</p>
-            <div className="contact-box">
-                <h4>Contact OMedia</h4>
-                <a href="mailto:contact@omedianetwork.com">contact@omedianetwork.com</a>
-                <p>Tel: 0224055999</p>
-                <p>Address: 10 Al Obour Buildings, Heliopolis, Cairo, Egypt</p>
+        <div className="container">
+            <div className="home-box">
+                <img src={logo} alt="OMedia" />
+                <p className="header-text">WOW is Comming Soon</p>
+                <div className="contact-box">
+                    <h4>Contact OMedia</h4>
+                    <a href="mailto:contact@omedianetwork.com">contact@omedianetwork.com</a>
+                    <p>Tel: 0224055999</p>
+                    <p>Address: 10 Al Obour Buildings, Heliopolis, Cairo, Egypt</p>
+                </div>
             </div>
         </div>
     )
